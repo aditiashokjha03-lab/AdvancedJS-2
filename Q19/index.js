ecfcccf67ffeb2f0f371ec105eca6d9e22ea27ed
Aditi Jha <aditiashokjha03@gmail.com>
2025-11-24 22:21:52 +0530

    Add age display and change functions

---
+++ Q19/index.js
@@ -0,0 +1,23 @@
+var age= 10;
+
+function displayAge() {
+  console.log("Display Age", age);
+}
+
+function changeAge() {
+  age++;
+  console.log("Change Age", age);
+}
+
+displayAge();      
+changeAge();  
+
+displayAge();      
+changeAge();
+
+displayAge();      
+changeAge();
+
+displayAge();      
+changeAge();
+
