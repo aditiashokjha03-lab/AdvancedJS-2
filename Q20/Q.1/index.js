dc917a73f9e40a714d314fc1437f62b70dcbce97
Aditi Jha <aditiashokjha03@gmail.com>
2025-11-24 22:35:57 +0530

    Implement counter functionality with increment and decrement

---
+++ Q20/Q.1/index.js
@@ -0,0 +1,36 @@
+function createCounter() {
+  let count = 0;
+
+  return {
+    increment: function() {
+      count++;
+      console.log("Current count:", count);
+    },
+    decrement: function() {
+      count--;
+      console.log("Current count:", count);
+    },
+    display: function() {
+      console.log("Current count:", count);
+    }
+  };
+}
+
+const counter = createCounter();
+counter.increment(); 
+counter.increment(); 
+counter.decrement(); 
+counter.display();  
+
+// multiple counters
+const counterA = createCounter();
+const counterB = createCounter();
+
+counterA.increment(); 
+counterA.increment(); 
+
+counterB.increment(); 
+counterB.decrement(); 
+
+counterA.display(); 
+counterB.display();  
